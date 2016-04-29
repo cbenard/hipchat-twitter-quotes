db58009829127a11a83d9eeab8aa534d654288a6
Chris Benard <chris@chrisbenard.net>
2016-04-29 15:40:08 -0500

    Fix autoload.

--- classes/autoload.php
+++ classes/autoload.php
@@ -3,6 +3,6 @@
 spl_autoload_register(function ($class) {
     $filename = __DIR__ . '/' . str_replace("\\", "/", $class) . '.php';
     if (file_exists($filename)) {
-        include(filename);
+        include($filename);
     }
 });
