079dfcfdd568d7b627e71caffe2e123412e7fbd9
Chris Benard <chris@chrisbenard.net>
2016-04-30 22:43:04 -0500

    More absolute path fixes.

--- common.php
+++ common.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once("config.php");
-require_once("vendor/autoload.php");
-require_once("classes/autoload.php");
-require_once("classes/dependency_injection.php");
+require_once(__DIR__ . "/config.php");
+require_once(__DIR__ . "/vendor/autoload.php");
+require_once(__DIR__ . "/classes/autoload.php");
+require_once(__DIR__ . "/classes/dependency_injection.php");
 
 date_default_timezone_set($config['global']['timezone']);
